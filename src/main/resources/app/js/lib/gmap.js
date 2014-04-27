54f0c6ea7b9cd81e5dcfa39f8e9e1deff542eaea
Alice Chen <alicechen@Alices-MacBook-Air.local>
2014-04-27 15:10:13 -0700

    Added JS libraries and CSS

---
+++ src/main/resources/app/js/lib/gmap.js
@@ -0,0 +1,5 @@
+define([
+	'async!https://maps.googleapis.com/maps/api/js?key=AIzaSyDY0kkJiTPVd2U7aTOAwhc9ySH6oHxOIYM&sensor=false&libraries=places,geometry'
+], function() {
+	return google.maps;
+});
