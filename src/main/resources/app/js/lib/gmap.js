022f075e53aebc2ce717326423abdb00c7094194
Alice Chen <alicechen@Alices-MacBook-Air.local>
2014-04-27 15:35:16 -0700

    Removed Google API key

--- src/main/resources/app/js/lib/gmap.js
+++ src/main/resources/app/js/lib/gmap.js
@@ -1,5 +1,5 @@
 define([
-	'async!https://maps.googleapis.com/maps/api/js?key=AIzaSyDY0kkJiTPVd2U7aTOAwhc9ySH6oHxOIYM&sensor=false&libraries=places,geometry'
+	'async!https://maps.googleapis.com/maps/api/js?sensor=false&libraries=places,geometry'
 ], function() {
 	return google.maps;
 });
