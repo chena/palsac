9a0b2f64ed33d71613ddf1b62db8071e44027c4c
Alice Chen <chiachun78@gmail.com>
2014-06-21 08:08:02 -0700

    Added chapter view, template and organizer template, working on adding multiple organizers

---
+++ src/main/resources/app/js/model/chapter.js
@@ -0,0 +1,84 @@
+define([
+	'backbone',
+	'bbnested',
+], function(Backbone, BackboneNested) {
+	
+	var Chapter = {};
+	
+	Chapter.Model = Backbone.Model.extend({
+		url: '/api/chapters',
+		
+		defaults: {
+			active: true
+		},
+		
+		relations: {
+			organizer: Backbone.Model,
+			venue: Backbone.Model,
+			events: Backbone.Collection
+		},
+		
+		setVenue: function(venue) {
+			this.set({
+				venue: venue
+			});
+		},
+		
+		setEvents: function(events) {
+			this.set({
+				events: events
+			});
+		},
+		
+		validate: function(attrs, options) {
+			var that = this,
+				badFields = [];
+			
+			var requiredFields = ['name', 'venue.address'];
+			
+			// validate required fields
+			requiredFields.forEach(function(field) {
+				// check if the given field is in a nested model
+				var nested = that.getNestedModel(field);
+				if (nested) {
+					var model = nested.nestedModel,
+						fieldName = nested.fieldName;
+					
+					if (!model || that.isEmpty(model.attributes[fieldName])) {
+						badFields.push(field);
+					}
+				} else {
+					if (that.isEmpty(attrs[field])) {
+						badFields.push(field);
+					}
+				}
+			});
+			
+			return badFields.length == 0 ? false : badFields;
+		 },
+		 
+		 getNestedModel: function(field) {
+			 if (field.indexOf('.') == -1) {
+				 return false;
+			 }
+			 var parts = field.split('.');
+			 return {
+				 modelName: parts[0],
+				 fieldName: parts[1],
+				 nestedModel: this.get(parts[0])
+			 };			 
+		 },
+		 
+		 isEmpty: function(value) {
+			 return !value || value.trim().length == 0;
+		 }
+	});
+	
+	Chapter.Collection = Backbone.Collection.extend({
+		url: '/api/chapters',
+		
+		model: Chapter.Model
+	});
+	
+	return Chapter;
+});
---
+++ src/main/resources/app/js/view/chapter.js
@@ -0,0 +1,197 @@
+define([
+	'backbone', 
+	'bbnested',
+	'jquery',
+	'underscore',
+	'mustache', 
+	'gmap', 
+	'timepicker', 
+	'moment',
+	'text!tpl/chapter.html',
+	'text!tpl/organizer.html'
+], function(Backbone, BackboneNested, $, _, Mustache, Gmap, TimePicker, Moment, ChapterTemplate, OrganizerTemplate) {
+	var EventView = Backbone.View.extend({
+		template: ChapterTemplate,
+		
+		events: {
+			'change input, textarea, select': 'changed',
+			'click .submit': 'submit',
+			'click div.has-error *': 'clearThisError',
+			'click .another': 'addOrganizer'
+		},
+		
+		render: function() {
+			this.$el.html(Mustache.render(this.template, this.model.toJSON()));
+			return this;
+		},
+		
+		autocomplete: undefined,
+		audocompleteAddr: undefined,
+		
+		initAddressAutocomplete: function() {
+			var that = this;
+			
+			// Gmap setup needs to be done after the view is rendered
+			that.autocomplete = new Gmap.places.Autocomplete($('#addressInput').get(0), {
+				types: ['geocode']
+			});
+			
+			var bindAddress =  function() {
+				var place = that.autocomplete.getPlace(),
+					geocode = place.geometry.location,
+					address = place.formatted_address;
+				
+				that.model.setVenue({
+					address: address,
+					latitude: geocode.lat(),
+					longtitude: geocode.lng()
+				});
+				
+				that.audocompleteAddr = address;
+			}
+			
+			Gmap.event.addListener(that.autocomplete, 'place_changed', bindAddress);
+		},
+		
+		codeAddress: function(address, callback) {
+			var that = this;
+			var geocoder = new Gmap.Geocoder();
+
+			geocoder.geocode({
+				'address': address
+			}, function(results, status) {
+				if (status == Gmap.GeocoderStatus.OK) {
+					var geocode = results[0];
+					console.log(status);
+					console.log(results);
+					
+					that.model.setVenue({
+						address: address,
+						latitude: geocode.lat(),
+						longtitude: geocode.lng()
+					});
+					
+					callback(true);
+				} else {
+					callback(false);
+				}
+			});
+		},
+		
+		venueChanged: function(event) {
+			this.changed(event, 'venue');
+		},
+		
+		organizerChanged: function(event) {
+			this.changed(event, 'organizer');
+		},
+		
+		changed: function(event) {
+			var target = event.target,
+				field = target.name,
+				value = target.value, 
+				changed = {};
+
+			var nested = this.model.getNestedModel(field);
+
+			if (nested) {
+				changed[nested.fieldName] = value;
+				var nestedModel = {}
+				nestedModel[nested.modelName] = changed; 
+				this.model.set(nestedModel);
+			} else {
+				changed[field] = value;
+				this.model.set(changed);
+				
+				// toggle form input based on event type
+				if (field === 'type') {
+					if (value === 'recurring') {
+						$('#eventDateInput').hide();
+						$('#eventDescInput').show();
+					} else {
+						$('#eventDescInput').hide();
+						$('#eventDateInput').show();
+					} 
+				}
+			}
+		},
+		
+		addOrganizer: function(event) {
+			event.preventDefault();
+			this.$('.another').remove(); // remove other add anchor
+			this.$('#organizers').append(Mustache.render(OrganizerTemplate));
+		},
+		
+		submit: function() {			
+			this.clearAllErrors();
+			var model = this.model;
+			
+			// client side validation
+			if (!model.isValid()) {
+				var requiredErrors = model.validationError;
+				console.log(requiredErrors);
+				requiredErrors.forEach(function(errorField) {
+					if (_.isObject(errorField)) {
+						var field = Object.keys(errorField)[0],
+							msg = errorField[field];
+						var parent = $('[name="' + field + '"]').parent().parent();
+						parent.addClass('has-error');
+						
+						// clear existing error msg
+						parent.find('span.help-block').remove();
+						
+						parent.append($('<span/>', {
+							text: msg,
+							'class': 'help-block'
+						}));
+					} else {
+						$('[name="' + errorField + '"]').parent().addClass('has-error');
+					}
+				});
+				
+				return;
+			}
+			
+			// address field is non-empty, validate it
+			var modelAddr = model.get('venue').get('address');
+			
+			console.log('model addr' + modelAddr);
+			console.log('autocomplete addr: ' + this.audocompleteAddr);
+			
+			if (!this.audocompleteAddr || this.audocompleteAddr != modelAddr) {
+				this.codeAddress(modelAddr, function(valid) {
+					if (!valid) {
+						console.log('invalid address!!');
+					}
+				});
+				
+			} 
+			
+			this.model.save(function(response) {
+				console.log(response);
+				Backbone.history.navigate('map', true);
+				$('.alert-success').show();
+				$('.alert-success').append($('<p/>', {
+					text: 'Your event has been successfully added'
+				}));
+				setTimeout(function() {
+					$('.alert-success').empty().hide();
+				}, 3000);
+			}); 
+		},
+		
+		clearThisError: function(event) {
+			var errorTarget = this.$(event.target).closest('.has-error');
+			errorTarget.removeClass('has-error');
+			errorTarget.find('.help-block').remove();
+		},
+		
+		clearAllErrors: function() {
+			this.$('.has-error').removeClass('has-error');
+			this.$('.help-block').remove();
+		}
+		
+	});
+	
+	return EventView;
+});
